e47d6dea5a4d24ea76fcc5b58f98a14d49839235
bredmond1019 <bredmond1019@gmail.com>
2022-02-17 12:48:55 -0500

    Updated Blog Page for Mobile

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,68 +1,65 @@
 import puppy from "./../images/puppy.jpeg";
-import blue_green from "./../images/blog_page/blue_green.avif";
-import mountain3 from "./../images/blog_page/mountain3.avif";
-import purple_triangle from "./../images/blog_page/purple_triangle.avif";
+import blue_green from "./../images/blog_page/blue_green.jpeg";
+import mountain3 from "./../images/blog_page/mountain3.jpeg";
+import purple_triangle from "./../images/blog_page/purple_triangle.jpeg";
 
 function Blog() {
+  let blogArticles = [
+    {
+      title: "From Teacher of Many to Teacher of One",
+      image: mountain3,
+      link: "https://dev.to/bredmond1019/from-teacher-of-many-to-teacher-of-one-tips-on-how-to-be-a-self-taught-194p",
+      description: "A Teacher's Tips on Being a Self-Taught Learner",
+    },
+    {
+      title: "Flask API Basics",
+      image: blue_green,
+      link: "#",
+      description: "React, GraphQL, Flask App -- Part 1",
+    },
+    {
+      title: "Flask Application Factory",
+      image: purple_triangle,
+      link: "#",
+      description: "React, GraphQL, Flask App -- Part 2",
+    },
+    {
+      title: "Flask & GraphQL",
+      image: mountain3,
+      link: "#",
+      description: "React, GraphQL, Flask App -- Part 3",
+    },
+  ];
+
   return (
     <div className="blog-page-wrapper">
       <div className="blog-main-page">
         <div className="blog-main-title-wrapper">
-          <h1 className="blog-main-title">Welcome to my blog page</h1>
-          <p className="blog-main-description">
-            As I write more blogs, I will continue to add them here
-          </p>
+          <h1 className="blog-main-title">Welcome to my blog</h1>
         </div>
         <div className="blog-articles-wrapper">
           <div className="blog-articles-info-wrapper">
-            <div className="blog-article-box-wrapper">
-              <div className="blog-article-image-wrapper">
-                <img src={mountain3} alt="" className="blog-article-image" />
-              </div>
-              <div className="blog-info-wrapper">
-                <h1 className="blog-article-title">Blog Article 1</h1>
-                <p className="blog-article-description">
-                  My blog article is about this and that. Also this. Mostly
-                  that.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-                  tenetur eveniet modi nobis non sed nisi dicta voluptas
-                  accusamus accusantium sunt laboriosam, similique adipisci
-                  sequi quidem dignissimos mollitia aut temporibus!
-                </p>
-              </div>
-            </div>
-
-            <div className="blog-article-box-wrapper">
-              <div className="blog-article-image-wrapper">
-                <img src={blue_green} alt="" className="blog-article-image" />
-              </div>
-              <div className="blog-info-wrapper">
-                <h1 className="blog-article-title">Blog Article 2</h1>
-                <p className="blog-article-description">
-                  My blog article is about this and that. Also this. Mostly
-                  that.
-                </p>
-              </div>
-            </div>
-
-            <div className="blog-article-box-wrapper">
-              <div className="blog-article-image-wrapper">
-                <img
-                  src={purple_triangle}
-                  alt=""
-                  className="blog-article-image"
-                />
-              </div>
-              <div className="blog-info-wrapper">
-                <h1 className="blog-article-title">Blog Article 3</h1>
-                <p className="blog-article-description">
-                  My blog article is about this and that. Also this. Mostly
-                  that.
-                </p>
-              </div>
-            </div>
+            {blogArticles.map((article, i) => {
+              return (
+                <div className="blog-article-box-wrapper">
+                  <div className="blog-article-image-wrapper">
+                    <img
+                      src={article.image}
+                      alt=""
+                      className="blog-article-image"
+                    />
+                  </div>
+                  <a className="blog-link" href={article.link}>
+                    <div className="blog-info-wrapper">
+                      <h1 className="blog-article-title">{article.title}</h1>
+                      <p className="blog-article-description">
+                        {article.description}
+                      </p>
+                    </div>
+                  </a>
+                </div>
+              );
+            })}
           </div>
           <div className="side-panel"></div>
         </div>
